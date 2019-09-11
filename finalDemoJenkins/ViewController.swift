6a5015cc30c51ba993af74a9d92410d29184f3ad
bharat <bharatshankar5>
2019-09-11 12:39:52 +0530

    added background colour

---
+++ finalDemoJenkins/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  finalDemoJenkins
+//
+//  Created by Bharat shankar on 11/09/19.
+//  Copyright © 2019 Bharat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
